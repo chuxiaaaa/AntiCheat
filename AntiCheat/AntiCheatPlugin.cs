0745a73b30035602b06c7ce993bd77e7e3c19d93
chuxiaaaa <1612121215@qq.com>
2024-02-25 01:27:32 +0800

    .

--- AntiCheat/AntiCheatPlugin.cs
+++ AntiCheat/AntiCheatPlugin.cs
@@ -15,7 +15,7 @@ namespace AntiCheat
     [BepInPlugin("AntiCheat", "AntiCheat", Version)]
     public class AntiCheatPlugin : BaseUnityPlugin
     {
-        public const string Version = "0.3.6";
+        public const string Version = "0.3.7";
         public static ManualLogSource ManualLog = null;
         public enum Language
         {
