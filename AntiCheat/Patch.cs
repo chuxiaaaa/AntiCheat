49b9d2a84d85225f1781dfd732d16fe84e2a2b8a
chuxiaaaa <1612121215@qq.com>
2024-02-24 14:30:11 +0800

    [/] Fix the issue where line breaks are not displayed correctly

--- AntiCheat/Patch.cs
+++ AntiCheat/Patch.cs
@@ -1829,12 +1829,13 @@ namespace AntiCheat
 
         [HarmonyPatch(typeof(MenuManager), "HostSetLobbyPublic")]
         [HarmonyPostfix]
-        public static void Start(MenuManager __instance,bool setPublic)
+        public static void Start(MenuManager __instance, bool setPublic)
         {
             if (setPublic)
             {
                 __instance.privatePublicDescription.text = LocalizationManager.GetString("tip_servername", new Dictionary<string, string>() {
-                    { "{text}",__instance.privatePublicDescription.text }
+                    { "{text}",__instance.privatePublicDescription.text },
+                    { "\\r\\n",Environment.NewLine }
                 });
             }
         }
