e1938f924009920cf07467505f5d3b696147bf1f
chuxiaaaa <1612121215@qq.com>
2025-11-08 06:24:08 +0800

    push

---
+++ AntiCheat/Plugin.cs
@@ -0,0 +1,12 @@
+﻿using BepInEx;
+
+using System;
+
+namespace LethalMod
+{
+    [BepInPlugin(LCMPluginInfo.PLUGIN_GUID, LCMPluginInfo.PLUGIN_NAME, LCMPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+
+    }
+}
